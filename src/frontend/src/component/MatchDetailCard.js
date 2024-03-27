598bf8aef9c3f6125fbea22fcf2501eb1874ae93
godhani gopal <godhanigopal@outlook.com>
2024-03-27 10:09:51 -0400

    completed UI elements for Team page
    ;

--- src/frontend/src/component/MatchDetailCard.js
+++ src/frontend/src/component/MatchDetailCard.js
@@ -1,13 +1,30 @@
-import { React } from "react";
+import React from "react";
+import "./MatchDetailCard.css";
+import { Link } from "react-router-dom";
 
-export const MatchDetailCard = ({ match }) => {
+export const MatchDetailCard = ({ teamName, match }) => {
   if (!match) return null;
+  const otherTeam = match.team1 === teamName ? match.team2 : match.team1;
+  const otherTeamRoute = `/teams/${otherTeam}`;
+
+  // Create a new Date object
+  const date = new Date(match.date);
+
+  // Format the date
+  const formattedDate = `${date.toLocaleString("default", {
+    month: "long",
+  })} ${date.getDate()}, ${date.getFullYear()}`;
+
   return (
     <div className="MatchDetailCard">
       <h2>Latest Matches</h2>
-      <h3>Match Details</h3>
+      <h2>
+        vs <Link to={otherTeamRoute}>{otherTeam}</Link>
+      </h2>
+      <h3>{formattedDate}</h3>
+      <h3>📍 at {match.venue}</h3>
       <h3>
-        {match.team1} vs {match.team2}
+        🏆 {match.matchWinner} 🏆 won by {match.resultMargin} {match.result}
       </h3>
     </div>
   );
--- src/frontend/src/pages/TeamPage.js
+++ src/frontend/src/pages/TeamPage.js
@@ -1,29 +1,34 @@
 import { React, useEffect, useState } from "react";
 import { MatchSmallCard } from "../component/MatchSmallCard";
 import { MatchDetailCard } from "../component/MatchDetailCard";
+import { useParams } from "react-router-dom";
 
 export const TeamPage = () => {
   const [team, setTeam] = useState({ matches: [] });
+  const { teamName } = useParams();
 
   useEffect(() => {
     const fetchMatches = async () => {
-      const response = await fetch(
-        "http://localhost:8080/team/Mumbai%20Indians"
-      );
+      const response = await fetch(`http://localhost:8080/team/${teamName}`);
       const data = await response.json();
       console.log(data);
       setTeam(data);
     };
     fetchMatches();
-  }, []);
-
+  }, [teamName]);
+  if (!team || !team.teamName) {
+    return <h2>Oooppss!! Team Not Found!</h2>;
+  }
   return (
     <div className="TeamPage">
       <h1>{team.teamName}</h1>
-      <MatchDetailCard match={team.matches[0]}></MatchDetailCard>
+      <MatchDetailCard
+        teamName={team.teamName}
+        match={team.matches[0]}
+      ></MatchDetailCard>
 
       {team.matches?.slice(1).map((match) => (
-        <MatchSmallCard match={match} />
+        <MatchSmallCard teamName={team.teamName} match={match} />
       ))}
     </div>
   );
